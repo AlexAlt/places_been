a1b1dc91a0bbe0adb76cacfaf98b8e8efc6520a6
Julianna Green & Alex Altieri <student@epicodus.com>
2015-08-11 11:34:12 -0700

    Adds spec to clear locations array before evaluating other specs

--- spec/places_been_spec.rb
+++ spec/places_been_spec.rb
@@ -2,6 +2,10 @@ require ('rspec')
 require ('places_been')
 
 describe(Places) do
+  before() do
+    Places.clear()
+  end
+  
   describe("#location") do
     it("let you give a location") do
       new_place = Places.new("Tennesse")
