e6a46ec2b0ca83785d5b9457aec0f14a30220ede
Julianna Green & Alex Altieri <student@epicodus.com>
2015-08-11 11:14:43 -0700

    Passes first spec to add new location

--- spec/places_been_spec.rb
+++ spec/places_been_spec.rb
@@ -2,8 +2,8 @@ require ('rspec')
 require ('places_been')
 
 describe(Places) do
-  describe('#location') do
-    it('returns location input') do
+  describe("#location") do
+    it("let you give a location") do
       new_place = Places.new("Tennesse")
       expect(new_place.location()).to(eq("Tennesse"))
     end
