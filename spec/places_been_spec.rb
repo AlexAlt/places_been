21d2f9f931ac04af03ddc9d72ef691b677d6a1b1
Julianna Green & Alex Altieri <student@epicodus.com>
2015-08-11 11:24:24 -0700

    Adds spec to test for save method

--- spec/places_been_spec.rb
+++ spec/places_been_spec.rb
@@ -14,4 +14,12 @@ describe(Places) do
       expect(Places.all()).to(eq([]))
     end
   end
+
+  describe('#save') do
+    it('saves locations into array') do
+      new_place = Places.new("Canada")
+      new_place.save()
+      expect(Places.all()).to(eq([new_place]))
+    end
+  end
 end
